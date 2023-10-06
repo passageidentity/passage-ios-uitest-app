61da64710bed74c7d7c98bed328eefcd4e3645b9
Ricky Padilla <rickycpadilla@gmail.com>
2023-10-06 10:49:18 -0600

    Initial Commit

---
+++ passage-ios-uitest-app/passage_ios_uitest_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  passage_ios_uitest_appApp.swift
+//  passage-ios-uitest-app
+//
+//  Created by Ricky Padilla on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct passage_ios_uitest_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
