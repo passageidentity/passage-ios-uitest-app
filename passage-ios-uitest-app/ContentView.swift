db01b61346184496927f1891f857aef4f79da948
Ricky Padilla <rickycpadilla@gmail.com>
2023-10-06 14:40:18 -0600

    Setup basic app UI for passkey tests.

--- passage-ios-uitest-app/ContentView.swift
+++ passage-ios-uitest-app/ContentView.swift
@@ -5,18 +5,56 @@
 //  Created by Ricky Padilla on 10/6/23.
 //
 
+import Passage
 import SwiftUI
 
 struct ContentView: View {
+    
+    let passage = PassageAuth(appId: Constants.appId)
+    
+    init() {
+        passage.overrideApiUrl(with: Constants.apiUrl)
+    }
+    
+    @State var identifier: String = ""
+    @State var isShowingSuccessAlert = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField(Constants.textFieldLabel, text: $identifier)
+                .textFieldStyle(.roundedBorder)
+            Button(Constants.registerPasskeyButton, action: registerWithPasskey)
+            Button(Constants.loginPasskeyButon, action: loginWithPasskey)
+            
+        }
+        .alert("Success", isPresented: $isShowingSuccessAlert) {
+            
         }
         .padding()
     }
+    
+    func registerWithPasskey() {
+        Task {
+            do {
+                let _ = try await passage.registerWithPasskey(identifier: identifier)
+                isShowingSuccessAlert = true
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+    func loginWithPasskey() {
+        Task {
+            do {
+                let _ = try await passage.loginWithPasskey()
+                isShowingSuccessAlert = true
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
 }
 
 #Preview {
---
+++ shared/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  passage-ios-uitest-app
+//
+//  Created by Ricky Padilla on 10/6/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let appId = "flsROGmPQ3f9rhyyckuUgBAe"
+    static let apiUrl = "https://auth-uat.passage.dev"
+    
+    static let textFieldLabel = "email"
+    static let registerPasskeyButton = "register with passkey"
+    static let loginPasskeyButon = "login with passkey"
+}
